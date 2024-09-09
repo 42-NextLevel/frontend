5eeb8ef9e703dec48137ee74ab89e66fd51cff5a
nijesmik <bkybb@duck.com>
2024-09-09 21:44:10 +0900

    :sparkles: Router

---
+++ src/library/router/Router.js
@@ -0,0 +1,35 @@
+import { virtualDOM } from '../dom/VirtualDOM.js';
+
+class Router {
+  constructor() {
+    this.routes = null;
+    window.addEventListener('popstate', () => {
+      virtualDOM.render(this.loadRoute());
+    });
+  }
+
+  setRoutes(routes) {
+    this.routes = routes;
+    return this;
+  }
+
+  loadRoute() {
+    const path = window.location.pathname;
+    const { element } = this.routes.find((route) => route.path === path);
+    return element;
+  }
+
+  navigate(path, options) {
+    this.updateHistory(path, options);
+    virtualDOM.render(this.loadRoute());
+  }
+
+  updateHistory(path, options) {
+    if (options && options.replace) {
+      return window.history.replaceState({}, '', path);
+    }
+    window.history.pushState({}, '', path);
+  }
+}
+
+export const $router = new Router();
---
+++ src/library/router/RouterProvider.js
@@ -0,0 +1,5 @@
+import { $router } from './Router.js';
+
+export const RouterProvider = ({ routes }) => {
+  return $router.setRoutes(routes).loadRoute();
+};
