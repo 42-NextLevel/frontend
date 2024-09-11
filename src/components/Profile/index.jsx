f945598fdae8ba87a02b9f7986b9fa8856c54b23
JunSeong <gentlmanjun@naver.com>
2024-09-11 14:58:41 +0900

    ✏️ profile css 클래스명 수정

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -2,7 +2,7 @@ import defaultImg from '/images/42_logo.png';
 
 const Profile = ({ image, intraId, nickname = '' }) => {
   return (
-    <div className='card border-3 text-center align-items-center justify-content-center profile-class'>
+    <div className='card border-3 text-center align-items-center justify-content-center profile'>
       <img
         src={image ? image : defaultImg}
         className='rounded-circle'
