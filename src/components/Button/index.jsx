b7a70d19929476864a6fb75819601e59cfd29d95
nijesmik <bkybb@duck.com>
2024-09-26 22:59:51 +0900

    :necktie: 버튼 프로퍼티 outline, noPadding 추가 (#27)

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,25 @@
-const Button = ({ children, onClick, disabled = false }) => {
+const Button = ({
+  children,
+  onClick,
+  disabled = false,
+  outline = false,
+  noPadding = false,
+}) => {
+  const styles = {
+    btn: true,
+    'btn-lg': true,
+    'btn-primary': !outline,
+    'btn-outline-primary': outline,
+    'px-5': !noPadding,
+  };
+  const className = Object.keys(styles)
+    .filter((key) => styles[key])
+    .join(' ');
+
   return (
     <button
       type='button'
-      className='btn btn-lg btn-primary'
+      className={className}
       onClick={onClick}
       disabled={disabled}
     >
