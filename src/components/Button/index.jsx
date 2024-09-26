22b93c80fc20c5c382e59c9cc5d50a8d119be420
nijesmik <bkybb@duck.com>
2024-09-26 17:11:02 +0900

    :necktie: 버튼 프로퍼티 disabled 추가

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,11 @@
-const Button = ({ children, onClick }) => {
+const Button = ({ children, onClick, disabled = false }) => {
   return (
-    <button type='button' className='btn btn-lg btn-primary' onClick={onClick}>
+    <button
+      type='button'
+      className='btn btn-lg btn-primary'
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
