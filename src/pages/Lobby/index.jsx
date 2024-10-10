a829e5371eeded4626a71bdbe66c4af44047afa0
JunSeong <gentlmanjun@naver.com>
2024-10-10 19:38:32 +0900

    ✨ 로비 모달 연결

--- src/pages/Lobby/index.jsx
+++ src/pages/Lobby/index.jsx
@@ -6,17 +6,22 @@ import { getRoomList } from '../../services/game';
 import { getUserProfile } from '../../services/user';
 import { useState, useEffect } from '@/library/hooks.js';
 import { getTestRoomList, getTestUserProfile } from '../../services/test';
+import ModalTrigger from '@/components/ModalTrigger';
+import JoinModal from './components/JoinModal';
+import CreateModal from './components/CreateModal';
+
+// TODO: 모달 끄면 input 초기화 => 모달 수정
 
 const Lobby = () => {
   const [roomList, setRoomList] = useState([]);
   const [userProfile, setUserProfile] = useState();
   const [page, setPage] = useState(1);
-  const [isPointerOver, setIsPointerOver] = useState(false);
   const [isThrottle, setIsThrottle] = useState(false);
+  const [selectedRoom, setSelectedRoom] = useState();
 
   const fetchRoomList = async () => {
-    // const response = [...(await getRoomList())];
-    const response = [...(await getTestRoomList())];
+    const response = [...(await getRoomList())];
+    // const response = [...(await getTestRoomList())];
     const slicedResponse = [];
 
     for (let i = 0; i < response.length; i += 4) {
@@ -29,35 +34,22 @@ const Lobby = () => {
   };
 
   const fetchUserProfile = async () => {
-    // const response = await getUserProfile();
-    const response = await getTestUserProfile();
+    const response = await getUserProfile();
+    // const response = await getTestUserProfile();
     setUserProfile(response);
   };
 
-  useEffect(async () => {
+  useEffect(() => {
     fetchRoomList();
     fetchUserProfile();
   }, []);
 
-  const handleMouseEnter = () => {
-    setIsPointerOver(true);
-    console.log('포인터가 요소 위에 있습니다.');
-  };
-
-  const handleMouseLeave = () => {
-    setIsPointerOver(false);
-    console.log('포인터가 요소를 벗어났습니다.');
-  };
-
   const handleWheel = (event) => {
-    if (isPointerOver && !isThrottle) {
-      console.log('마우스 휠 이벤트 발생');
+    if (!isThrottle) {
       setIsThrottle(true);
       if (event.deltaY > 0) {
-        // 휠을 아래로 스크롤
         setPage((prevPage) => (prevPage < roomList.length ? prevPage + 1 : 1));
       } else {
-        // 휠을 위로 스크롤
         setPage((prevPage) => (prevPage > 1 ? prevPage - 1 : roomList.length));
       }
       setTimeout(() => {
@@ -66,60 +58,86 @@ const Lobby = () => {
     }
   };
 
+  const handleJoinRoom = (roomInfo) => {
+    setSelectedRoom(roomInfo);
+  };
+
   return (
-    <div
-      className='position-absolute top-50 start-50 translate-middle'
-      style='width: 1024px; height: 100%;'
-    >
-      <div>
-        <div className='d-flex justify-content-between col-9 mb-4 pe-3'>
-          <h2 className='mb-0'>방 리스트</h2>
-          <div>
-            <button className='btn btn-primary px-5 py-2 rounded-3 me-3'>
-              <img src={AddRoundIcon} alt='add-round-icon' />
-              <span>방 만들기</span>
-            </button>
-            <button
-              className='btn btn-primary p-2 rounded-3'
-              onClick={fetchRoomList}
-            >
-              <img src={RefreshIcon} alt='refresh-icon' />
-            </button>
-          </div>
-        </div>
-        <div className='row mx-0 gx-0'>
-          <div
-            className='col-9'
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            onWheel={handleWheel}
-          >
-            <div className='row mx-0' style={{ height: '294px' }}>
-              {/* 방 리스트 */}
-              {roomList[page - 1]?.map((roomInfo, index) => (
-                <div
-                  key={roomInfo.id + 'a'}
-                  className={`col-6 px-0 pe-3 ${index < 2 ? 'pb-3' : ''}`}
-                >
-                  <RoomCard {...roomInfo} />
-                </div>
-              ))}
+    <>
+      <div
+        className='position-absolute top-50 start-50 translate-middle'
+        style='width: 1024px; height: 100%; padding-top: 100px;'
+      >
+        <div>
+          <div className='d-flex justify-content-between col-9 mb-4 pe-3'>
+            <h2 className='mb-0'>방 리스트</h2>
+            <div>
+              <ModalTrigger id='create'>
+                <button className='btn btn-primary px-5 py-2 rounded-3 me-3'>
+                  <img src={AddRoundIcon} alt='add-round-icon' />
+                  <span>방 만들기</span>
+                </button>
+              </ModalTrigger>
+              <button
+                className='btn btn-primary p-2 rounded-3'
+                onClick={fetchRoomList}
+              >
+                <img src={RefreshIcon} alt='refresh-icon' />
+              </button>
             </div>
           </div>
-          <div className='col-3'>
-            {/* 프로필 */}
-            <Profile {...userProfile} />
-            <button type='button' className='btn btn-secondary py-2 mt-3 w-100'>
-              로그아웃
-            </button>
+          <div className='row mx-0 gx-0'>
+            <div className='col-9' onWheel={handleWheel}>
+              <div className='row mx-0' style='height: 294px'>
+                {/* 방 리스트 */}
+                {roomList[page - 1]?.map((roomInfo, index) => (
+                  <div
+                    key={roomInfo.id}
+                    className={`col-6 px-0 pe-3 ${index < 2 ? 'pb-3' : ''}`}
+                    onClick={() => handleJoinRoom(roomInfo)}
+                  >
+                    <ModalTrigger id='join'>
+                      <RoomCard {...roomInfo} />
+                    </ModalTrigger>
+                  </div>
+                ))}
+              </div>
+              <div className='d-flex justify-content-center pe-3 pt-2'>
+                {/* 페이지네이션 */}
+                {Array.from({ length: roomList.length }, (_, index) => (
+                  <div
+                    key={index}
+                    className={`mx-1 rounded-circle ${page === index + 1 ? 'bg-primary' : 'border border-primary'}`}
+                    style='width: 10px; height: 10px; cursor: pointer;'
+                    onClick={() => setPage(index + 1)}
+                  />
+                ))}
+              </div>
+            </div>
+            <div className='col-3'>
+              {/* 프로필 */}
+              <Profile {...userProfile} />
+              <button
+                type='button'
+                className='btn btn-secondary py-2 mt-3 w-100'
+              >
+                로그아웃
+              </button>
+            </div>
           </div>
+        </div>
+        <div>
+          <h2>경기 기록</h2>
+          <div>경기 기록 창</div>
         </div>
       </div>
       <div>
-        <h2>경기 기록</h2>
-        <div>경기 기록 창</div>
+        {/* 게임 생성 모달 */}
+        <CreateModal id='create' />
+        {/* 게임 참가 모달 */}
+        <JoinModal {...selectedRoom} id='join' />
       </div>
-    </div>
+    </>
   );
 };
 
