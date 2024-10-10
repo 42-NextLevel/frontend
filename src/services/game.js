962f64d29bec472b6a0871deeaf1aa1a018a5eea
JunSeong <gentlmanjun@naver.com>
2024-10-10 19:35:50 +0900

    ✨ 게임 api 연결

--- src/services/game.js
+++ src/services/game.js
@@ -1,5 +1,29 @@
 import { service } from './index';
 
+export const getHistory = () => {
+  return service.get('/history');
+};
+
 export const getRoomList = async () => {
   return service.get('/list').then((res) => res.data);
+};
+
+export const postMakeRoom = async (data) => {
+  return service.post('/new', data).then((res) => res.data);
+};
+
+export const postJoinRoom = async (data) => {
+  return service.post('/join', data).then((res) => res.data);
+};
+
+export const getRoomInfo = () => {
+  return service.get('/info');
+};
+
+export const postGameStart = () => {
+  return service.get('/start');
+};
+
+export const getUserInfo = () => {
+  return service.get('/user-info');
 };
