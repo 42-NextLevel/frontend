faf680b646bf9acd7decae4606a12ae478a891bb
JunSeong <gentlmanjun@naver.com>
2024-10-24 15:42:06 +0900

    🐛 변수명을 profile api response에 맞게 수정 (#38)

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,14 +1,14 @@
 import defaultImg from '/images/42_logo.png';
 
-const Profile = ({ image, intraId, nickname = '' }) => {
+const Profile = ({ profile_image, intra_id, nickname = '' }) => {
   return (
     <div className='card ratio-1 d-flex flex-column text-center align-items-center justify-content-center'>
       <img
-        src={image ? image : defaultImg}
+        src={profile_image ? profile_image : defaultImg}
         className='object-fit-cover profile-image ratio-1 rounded-circle mt-2'
         alt='profile_image'
       />
-      <div className='pt-3 pb-2'>@{intraId}</div>
+      <div className='pt-3 pb-2'>@{intra_id}</div>
       {nickname && <h5>{nickname}</h5>}
     </div>
   );
