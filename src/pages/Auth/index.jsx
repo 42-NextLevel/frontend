55d9e67168b1a9812358bdb36f8838f8c677b7b8
nijesmik <bkybb@duck.com>
2024-11-07 01:44:29 +0900

    :necktie: Auth 페이지 임의 접근 시 예외 처리

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,20 +1,31 @@
 import { useNavigate, useSearchParams } from '@/library/router/hooks.js';
 import { useEffect } from '@/library/hooks.js';
 import { post42Code } from '@/services/auth.js';
+import PageNotFound from '@/pages/404/index.jsx';
 
 const Auth = () => {
   const searchParams = useSearchParams();
+  const code = searchParams.get('code');
+  if (!code) {
+    return <PageNotFound />;
+  }
+
   const navigate = useNavigate();
   const handleRedirect = () => {
     if (searchParams.get('error')) {
       return navigate('/', { replace: true });
     }
-    post42Code(searchParams.get('code')).then(({ registered }) => {
-      if (registered) {
-        return navigate('/auth/code', { replace: true });
-      }
-      return navigate('/auth/mail', { replace: true });
-    });
+    post42Code(code)
+      .then(({ registered }) => {
+        if (registered) {
+          return navigate('/auth/code', { replace: true });
+        }
+        return navigate('/auth/mail', { replace: true });
+      })
+      .catch(() => {
+        alert('잘못된 접근입니다.');
+        navigate('/', { replace: true });
+      });
   };
 
   useEffect(() => {
