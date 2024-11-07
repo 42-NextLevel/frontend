8bb72fcacbd0d81f0351fea32b519f9b831a686c
JunSeong <gentlmanjun@naver.com>
2024-11-07 19:23:33 +0900

    ✨ lobby loader에서 game history 로드

--- src/services/lobby.js
+++ src/services/lobby.js
@@ -1,11 +1,13 @@
 import { getRoomList } from '@/services/game';
 import { getUserProfile } from '@/services/user';
+import { getGameHistory } from './game';
 
 export const lobbyLoader = async () => {
-  const [roomList, userProfile] = await Promise.all([
+  const [roomList, userProfile, gameHistory] = await Promise.all([
     getRoomList(),
     getUserProfile(),
+    getGameHistory(),
   ]);
 
-  return { roomList, userProfile };
+  return { roomList, userProfile, gameHistory };
 };
